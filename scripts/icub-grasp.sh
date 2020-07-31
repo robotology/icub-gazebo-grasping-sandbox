f19685b309d2aeedf8e16bfdb3d69d671324a34e
Ugo Pattacini <ugo.pattacini@iit.it>
2020-07-31 10:56:12 +0200

    fixed loading of XML apps

--- scripts/icub-grasp.sh
+++ scripts/icub-grasp.sh
@@ -9,9 +9,9 @@ yarp wait /icubSim/head/state:o
 yarp wait /icubSim/left_arm/state:o
 yarp wait /icubSim/right_arm/state:o
 
-yarpmanager-console --application icub-grasp.xml --run --connect --exit --silent
+yarpmanager-console --application ${ICUBcontrib_DIR}/share/ICUBcontrib/applications/icub-grasp.xml --run --connect --exit --silent
 sleep 10
-yarpmanager-console --application icub-grasp.xml --kill --exit --silent
+yarpmanager-console --application ${ICUBcontrib_DIR}/share/ICUBcontrib/applications/icub-grasp.xml --kill --exit --silent
 
 declare -a modules=("gzclient" "gzserver" "yarpserver")
 for module in ${modules[@]}; do
