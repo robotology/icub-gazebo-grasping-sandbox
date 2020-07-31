d2834331242404574483c388bb6112f2ee3ee208
Ugo Pattacini <ugo.pattacini@iit.it>
2020-07-31 16:40:38 +0000

    added tf stuff

--- scripts/icub-grasp.sh
+++ scripts/icub-grasp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 yarpserver --write --silent &
+yarpdev --device transformServer --ROS::enable_ros_publisher false --ROS::enable_ros_subscriber false
 gazebo -e dart icub-gazebo-grasping-sandbox.sdf &
 
 sleep 5
@@ -12,7 +13,7 @@ yarp wait /icubSim/right_arm/state:o
 yarpmanager-console --application ${ICUBcontrib_DIR}/share/ICUBcontrib/applications/icub-grasp.xml --run --connect --exit --silent
 
 sleep 10
-declare -a modules=("yarpview" "iKinGazeCtrl" "iKinCartesianSolver" "yarprobotinterface" "gzclient" "gzserver" "yarpserver")
+declare -a modules=("yarpview" "iKinGazeCtrl" "iKinCartesianSolver" "yarprobotinterface" "gzclient" "gzserver" "yarpdev" "yarpserver")
 for module in ${modules[@]}; do
   killall ${module}
 done
