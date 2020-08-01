8c523b83afdb3adfc532126ec43aa6b5498ed0d1
Ugo Pattacini <ugo.pattacini@iit.it>
2020-08-01 10:51:19 +0000

    preprogrammed grasp

--- scripts/icub-grasp.sh
+++ scripts/icub-grasp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yarpserver --write --silent &
-yarpdev --device transformServer --ROS::enable_ros_publisher false --ROS::enable_ros_subscriber false
+yarpdev --device transformServer --ROS::enable_ros_publisher false --ROS::enable_ros_subscriber false & 
 gazebo -e dart icub-gazebo-grasping-sandbox.sdf &
 
 sleep 5
@@ -11,9 +11,16 @@ yarp wait /icubSim/left_arm/state:o
 yarp wait /icubSim/right_arm/state:o
 
 yarpmanager-console --application ${ICUBcontrib_DIR}/share/ICUBcontrib/applications/icub-grasp.xml --run --connect --exit --silent
+yarp wait /icub-grasp/rpc
 
-sleep 10
-declare -a modules=("yarpview" "iKinGazeCtrl" "iKinCartesianSolver" "yarprobotinterface" "gzclient" "gzserver" "yarpdev" "yarpserver")
+sleep 5
+echo "grasp" | yarp rpc /icub-grasp/rpc
+
+sleep 5
+declare -a modules=("icub-gazebo-grasping-sandobox" \
+                    "yarpview" "iKinGazeCtrl" "iKinCartesianSolver" \
+                    "yarprobotinterface" "gzclient" "gzserver" \
+                    "yarpdev" "yarpserver")
 for module in ${modules[@]}; do
   killall ${module}
 done
