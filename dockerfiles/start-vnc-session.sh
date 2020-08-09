8b2ca2de38b205145e3914d667397c80bab99d21
Ugo Pattacini <ugo.pattacini@iit.it>
2020-08-09 16:45:27 +0200

    added -loop to vnc

--- dockerfiles/start-vnc-session.sh
+++ dockerfiles/start-vnc-session.sh
@@ -1,4 +1,4 @@
 X :1 -config /etc/X11/xorg.conf &
 startxfce4 &
-x11vnc -display :1 -N -forever -shared &
+x11vnc -display :1 -N -forever -loop -shared &
 /opt/novnc/utils/launch.sh --web /opt/novnc --vnc 0.0.0.0:5901 --listen 6080 &
