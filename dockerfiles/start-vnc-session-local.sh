14bef8180e4c8d2e5ac5cd3178d4904d22781915
Ugo Pattacini <ugo.pattacini@iit.it>
2020-08-17 10:39:13 +0200

    [docker] streamline vnc

--- dockerfiles/start-vnc-session-local.sh
+++ dockerfiles/start-vnc-session-local.sh
@@ -1,4 +1,6 @@
-sudo X :1 -config /etc/X11/xorg.conf &
-startxfce4 &
-x11vnc -display :1 -N -forever -loop -shared &
-/opt/novnc/utils/launch.sh --web /opt/novnc --vnc 0.0.0.0:5901 --listen 6080 &
+pkill -9 -f "vnc" && pkill -9 -f "xf" && sudo pkill -9 Xorg
+sudo rm -f /tmp/.X1-lock
+sudo nohup X :1 -config /etc/X11/xorg.conf > /dev/null 2>&1 &
+nohup startxfce4 > /dev/null 2>&1 &
+nohup x11vnc -display :1 -N -forever -loop -shared > /dev/null 2>&1 &
+nohup /opt/novnc/utils/launch.sh --web /opt/novnc --vnc 0.0.0.0:5901 --listen 6080 > /dev/null 2>&1 &
