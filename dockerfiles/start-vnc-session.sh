e2f222764ba972829e69a5d8cfc813883d81773f
Ugo Pattacini <ugo.pattacini@iit.it>
2020-08-17 18:31:57 +0200

    [docker] cleanup

--- dockerfiles/start-vnc-session.sh
+++ dockerfiles/start-vnc-session.sh
@@ -1,6 +1,6 @@
 pkill -9 -f "vnc" && pkill -9 -f "xf" && pkill -9 Xorg
 rm -f /tmp/.X1-lock
-nohup X :1 -config /etc/X11/xorg.conf > /dev/null 2>&1 &
+nohup X ${DISPLAY} -config /etc/X11/xorg.conf > /dev/null 2>&1 &
 nohup startxfce4 > /dev/null 2>&1 &
-nohup x11vnc -display :1 -N -forever -loop -shared > /dev/null 2>&1 &
-nohup /opt/novnc/utils/launch.sh --web /opt/novnc --vnc 0.0.0.0:5901 --listen 6080 > /dev/null 2>&1 &
+nohup x11vnc -localhost -display ${DISPLAY} -N -forever -shared -bg > /dev/null 2>&1
+nohup /opt/novnc/utils/launch.sh --web /opt/novnc --vnc localhost:5901 --listen 6080 > /dev/null 2>&1 &
