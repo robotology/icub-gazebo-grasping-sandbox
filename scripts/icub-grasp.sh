900ed6ff5c1ae4e7e884333f4b3dc0b0af06588c
Ugo Pattacini <ugo.pattacini@iit.it>
2020-10-02 06:46:36 +0000

    scripting cleanup

--- scripts/icub-grasp.sh
+++ scripts/icub-grasp.sh
@@ -3,17 +3,9 @@
 # launch the demo
 run() {
     yarpserver --write --silent &
-    gazebo -e dart icub-gazebo-grasping-sandbox.sdf &
-
-    sleep 20
-    yarp wait /icubSim/torso/state:o
-    yarp wait /icubSim/head/state:o
-    yarp wait /icubSim/left_arm/state:o
-    yarp wait /icubSim/right_arm/state:o
-
     yarpmanager-console --application ${ICUBcontrib_DIR}/share/ICUBcontrib/applications/icub-grasp.xml --run --connect --exit --silent
+    
     yarp wait /icub-grasp/rpc
-
     echo "go" | yarp rpc /icub-grasp/rpc
 
     sleep 5
