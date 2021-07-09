b712c2ad4f96dd4fe0d49ba7b674cf308c7670b9
Ugo Pattacini <ugo.pattacini@iit.it>
2021-07-09 11:29:40 +0200

    Update start-vnc-session.sh

--- dockerfiles/start-vnc-session.sh
+++ dockerfiles/start-vnc-session.sh
@@ -3,4 +3,4 @@ sudo rm -f /tmp/.X1-lock
 sudo nohup X ${DISPLAY} -config /etc/X11/xorg.conf > /dev/null 2>&1 &
 nohup startxfce4 > /dev/null 2>&1 &
 nohup x11vnc -localhost -display ${DISPLAY} -N -forever -shared -bg > /dev/null 2>&1
-nohup /opt/novnc/utils/launch.sh --web /opt/novnc --vnc localhost:5901 --listen 6080 > /dev/null 2>&1 &
+nohup /opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5901 --listen 6080 > /dev/null 2>&1 &
