29f26eeec7525c42c50a626ff1100f432db75b40
Ugo Pattacini <ugo.pattacini@iit.it>
2022-09-10 20:18:32 +0200

    Update icub-grasp.sh

--- scripts/icub-grasp.sh
+++ scripts/icub-grasp.sh
@@ -10,7 +10,7 @@
 # launch the demo
 run() {
     yarpserver --write --silent &
-    yarpmanager-console --application /usr/local/share/ICUBcontrib/applications/icub-grasp.xml --run --connect --exit --silent
+    yarpmanager-console --application ${ROBOTOLOGY_SUPERBUILD_INSTALL_DIR}/share/ICUBcontrib/applications/icub-grasp.xml --run --connect --exit --silent
     
     yarp wait /icub-grasp/rpc
     echo "go" | yarp rpc /icub-grasp/rpc
